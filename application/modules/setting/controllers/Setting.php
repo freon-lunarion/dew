356ed385d0c6e3d2f092f8d1a8a21ee0e43b14ab
freon-lunarion <antonio.daniswara@gmail.com>
2017-07-03 19:51:51 +1000

    .

--- application/modules/setting/controllers/Setting.php
+++ application/modules/setting/controllers/Setting.php
@@ -11,7 +11,7 @@ class Setting extends CI_Controller{
 
   function index()
   {
-
+    $this->load->view('home_view');
   }
 
 }
---
+++ application/modules/setting/views/home_view.php
@@ -0,0 +1,7 @@
+<?php $this->load->view('_base/top.php'); ?>
+<ul class="nav nav-pills nav-stacked">
+  <li><?php echo anchor('Setting/Perspective','Perspective')?></li>
+  <li><?php echo anchor('Setting/Measurement','Measurement')?></li>
+  <li><?php echo anchor('Setting/Formula','Formula')?></li>
+</ul>
+<?php $this->load->view('_base/bottom.php'); ?>
